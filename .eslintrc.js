cca5c8fab5a384760ac4d8e6fdf90e5adfa70577
Kim Dohee <kimdohee@Kimui-MacBookPro.local>
2021-02-21 03:49:33 +0900

    :wrench: set up lint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,37 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'comma-dangle': ['warn', 'always-multiline'],
+    'arrow-parens': ['warn', 'as-needed'],
+    'eol-last': ['warn', 'always'],
+    'no-unused-vars': ['warn', { args: 'none' }],
+    quotes: ['warn', 'single'],
+    semi: ['warn', 'always'],
+    'space-before-function-paren': [
+      'warn',
+      {
+        anonymous: 'always',
+        named: 'never',
+        asyncArrow: 'always',
+      },
+    ],
+    'space-before-blocks': ['warn', 'always'],
+    'no-process-env': ['warn'],
+  },
 };
