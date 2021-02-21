ae08c583d8e3e18fbb085665ead85f1e005d4ce8
Kim Dohee <kimdohee@Kimui-MacBookPro.local>
2021-02-21 18:15:36 +0900

    :art: make modal components for todo details

---
+++ src/ModalContainer.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { StyleSheet, Modal, View } from 'react-native';
+
+const ModalContainer = ({ children, isModalOpen, setModalOpen }) => {
+  return (
+    <Modal
+      animationType="slide"
+      transparent={true}
+      visible={isModalOpen}
+      onRequestClose={() => setModalOpen(false)}>
+      <View style={styles.centeredView}>
+        <View style={styles.modalView}>
+          {children}
+        </View>
+      </View>
+    </Modal>
+  );
+};
+
+const styles = StyleSheet.create({
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: 'rgba(0,0,0,0.5)',
+  },
+  modalView: {
+    height: '60%',
+    width: '80%',
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+});
+
+export default ModalContainer;
